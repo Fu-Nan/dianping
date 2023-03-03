bfca2c05a874407f375f2bf21d41dc26e10486fd
fn <1186174184@qq.com>
2023-03-03 18:09:56 +0800

    分布式锁v2.0，解决锁误删问题中，unlock不能保证原子性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,13 +1,23 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";   //true删除uuid的下划线
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
@@ -40,16 +50,25 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unLock() {
-        //获取当前lockId
-        long threadId = Thread.currentThread().getId();
-        String lockId = ID_PREFIX + threadId;
-
-        //获取Redis里锁的Id
-        String redisLockId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        //判断是否是同一把锁
-        if (lockId.equals(redisLockId)) {
-            //是同一把锁，才解锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用Lua脚本，解决线程安全问题
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
     }
+//    @Override
+//    public void unLock() {
+//        //获取当前lockId
+//        long threadId = Thread.currentThread().getId();
+//        String lockId = ID_PREFIX + threadId;
+//
+//        //获取Redis里锁的Id
+//        String redisLockId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        //判断是否是同一把锁
+//        if (lockId.equals(redisLockId)) {
+//            //是同一把锁，才解锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
